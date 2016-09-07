e772080f299bdaeb6f1662bfe8b3a04cf99fccc7
Adam Libunao <alibunao@mac.com>
2016-09-07 10:49:16 -0400

    first commit with a lot of info

---
+++ CheckList/CheckListItem.swift
@@ -0,0 +1,19 @@
+//
+//  CheckListItem.swift
+//  CheckList
+//
+//  Created by Adam Libunao on 2016-09-06.
+//  Copyright © 2016 Adamazing. All rights reserved.
+//
+
+import Foundation
+
+class ChecklistItem {
+    var text = ""
+    var checked = false
+
+
+func toggleChecked() {
+    checked = !checked
+ }
+}
